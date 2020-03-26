9308f30ca144c1bf7b6e45097c9197624cf00b19
adityaraj079 <55791929+adityaraj079@users.noreply.github.com>
2020-03-26 23:08:10 +0530

    Update Code1.cpp

--- Code1.cpp
+++ Code1.cpp
@@ -11,7 +11,7 @@ int add(int a,int b)
 float add(float a, float b)
 {
 	return (a+b);
-}
+}                                     //This is for addition
 void main()
 {
 	int x=4,y=5;
@@ -19,4 +19,28 @@ void main()
 	add();
 	add(x,y);
 	add(m,n);
+}                                     //This is main function
+
+//From here changes are from Aditya RAJ
+
+int sub()
+{
+	return (2-3);
 }
+int sub(int a,int b)
+{
+	return (a-b);
+}
+float add(float a, float b)
+{
+	return (a-b);
+}                                     //This is for substraction
+void main()
+{
+	int x=4,y=5;
+	float m=3.2,n=7.2;
+	sub();
+	sub(x,y);
+	sub(m,n);
+}                                     //This is main function
+
