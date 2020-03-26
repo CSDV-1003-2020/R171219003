3b8baf42dccbe0c7c9c817281ffa4bb9cf3f09bb
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-03-26 22:50:00 +0530

    modification done int datatype removed

--- Code1.cpp
+++ Code1.cpp
@@ -4,9 +4,6 @@ int add()
 {
 	return (2+3);
 }
-int add(int a,int b)
-{
-	return (a+b);
 }
 float add(float a, float b)
 {
@@ -14,9 +11,7 @@ float add(float a, float b)
 }
 void main()
 {
-	int x=4,y=5;
 	float m=3.2,n=7.2;
 	add();
-	add(x,y);
 	add(m,n);
 }
