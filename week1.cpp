48e0b31068cc8bb1c54e2713b1fb3c684eb5dd63
Akshat <akshatgoyal1018@gmail.com>
2020-04-02 23:26:45 +0530

    Added comments for every for loop

--- week1.cpp
+++ week1.cpp
@@ -4,15 +4,15 @@ void main()
 {
 	int a[10]={1,2,3,4,5,6,7,8,9,10};
 	int b[10]={0};
-	for(int k=0;k<10;k++)
+	for(int k=0;k<10;k++)    //to show array a
 	{
 		cout<<a[k]<<endl;
 	}	
-	for(int i=0;i<10;1++)
+	for(int i=0;i<10;1++)    //to reverse the a to b
 	{
 		b[10-i]=a[i];
 	}
-	for(int j=0;j<10;j++)
+	for(int j=0;j<10;j++)    //to show array b
 	{
 		cout<<"\n"<<b[j];
 	}	
