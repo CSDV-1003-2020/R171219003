854cd30f3d9b63ece87042578efd273041f09ff9
Akshat <akshatgoyal1018@gmail.com>
2020-04-02 22:32:10 +0530

    Programe for a array to reverse

---
+++ week1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int a[10]={1,2,3,4,5,6,7,8,9,10};
+	int b[10]={0};
+	for(int k=0;k<10;k++)
+	{
+		cout<<a[k]<<endl;
+	}	
+	for(int i=0;i<10;1++)
+	{
+		b[10-i]=a[i];
+	}
+	for(int j=0;j<10;j++)
+	{
+		cout<<"\n"<<b[j];
+	}	
+}	
